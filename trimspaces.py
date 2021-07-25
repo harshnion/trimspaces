b8f92f30610e501d658d0d3558df1359c0fef0b3
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-25 20:42:29 +0530

    321

---
+++ trimspaces.py
@@ -0,0 +1,6 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+def remove(string):
+    return "".join(string.split())
+string=input()
+print(remove(string))
